921cd9e7f759ee705febf48d4d3a633ab5cacd7e
Rish777 <56560637+Rish777@users.noreply.github.com>
2020-01-12 23:26:32 +0530

    Calling one constructor form another constructor of same class using this() keyword

--- Important/src/Demo/This.java
+++ Important/src/Demo/This.java
@@ -10,12 +10,14 @@ public class This {
 	}
 	public This(int a)
 	{
-		System.out.println("this is first constructor"+a);
+		
+		System.out.println("this is first constructor"+ a);
 		
 	}
 	
 	public This(String a)
 	{
+		
 		this(100);
 		System.out.println(a);
 	}
