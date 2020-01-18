905ebf8b8e834694cc3411fed285318ff0c19405
Rish777 <56560637+Rish777@users.noreply.github.com>
2020-01-19 01:29:47 +0530

    To replace all the numbers with 'x' except last 4 in a String (like bank account number-xxxxxxxxxxxx1234)

---
+++ Important/src/Demo/Duplicate.java
@@ -0,0 +1,32 @@
+package Demo;
+import java.util.*;
+
+public class Duplicate {
+
+	public static void main(String[] args) 
+	{	
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Enter account number");
+		String s=scan.nextLine();
+		scan.close();
+		for(int i=0;i<s.length()-4;i++)
+		{
+			char c=s.charAt(i);
+			String s1=Character.toString(c);
+			
+			String s2=s1.replaceAll("[0-9]","x");
+			String s3=s2.replaceAll(" ","");
+			System.out.print(s3);
+		}
+		for(int j=s.length()-4;j<s.length();j++)
+		{
+			char c1=s.charAt(j);
+			String s4=Character.toString(c1);
+			System.out.print(s4);
+		}
+		
+		
+
+	}
+
+}
